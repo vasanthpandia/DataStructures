2f554f4d0445150a664ae927354bebc7cc2cdf2f
Vasanth Pandiarajan <itsmevasanth@hotmail.com>
2019-09-15 15:43:29 -0500

    Trie Data Structures Implemented

---
+++ DataStructures/TNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    class TNode
+    {
+        public Dictionary<char, TNode> children;
+        public char item;
+        public bool endOfWord;
+
+        public TNode()
+        {
+            children = new Dictionary<char, TNode>();
+            this.endOfWord = false;
+        }
+    }
+}
---
+++ DataStructures/Trie.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    class Trie
+    {
+        private readonly TNode root;
+
+        public Trie()
+        {
+            root = new TNode();
+        }
+
+        public void insert(string[] words)
+        {
+            foreach (string word in words)
+                insert(word);
+        }
+
+        public void insert(string s)
+        {
+            insert(s.ToCharArray());
+        }
+
+        public void insert(char[] s) // Iterative
+        {
+            TNode current = root;
+            foreach(char c in s)
+            {
+                TNode node;
+                current.children.TryGetValue(c, out node);
+                if(node == null)
+                {
+                    node = new TNode();
+                    current.children[c] = node;
+                }
+                current = node;
+            }
+            current.endOfWord = true;
+        }
+
+        public void insertRecursive(string word)
+        {
+            insertRecursive(root, word, 0);
+        }
+
+        public void insertRecursive(TNode current, string word, int index)
+        {
+            if(index == word.Length)
+            {
+                current.endOfWord = true;
+                return;
+            }
+
+            TNode node;
+            current.children.TryGetValue(word[index], out node);
+            if (node == null)
+            {
+                node = new TNode();
+                current.children[word[index]] = node;
+            }
+
+            insertRecursive(node, word, index + 1);
+        }
+
+        public bool searchRecursive(string word)
+        {
+            return searchRecursive(root, word, 0);
+        }
+
+        public bool searchRecursive(TNode current, string word, int index)
+        {
+            if (index == word.Length)
+            {
+                return current.endOfWord;
+            }
+
+            TNode node;
+            current.children.TryGetValue(word[index], out node);
+
+            if (node == null) return false;
+
+            return searchRecursive(node, word, index + 1);
+        }
+
+        public bool search(string word)
+        {
+            TNode current = root;
+
+            foreach(char c in word.ToCharArray())
+            {
+                TNode node;
+                current.children.TryGetValue(c, out node);
+
+                if(node == null)
+                {
+                    return false;
+                }
+
+                current = node;
+            }
+
+            return current.endOfWord;
+        }
+    }
+}
